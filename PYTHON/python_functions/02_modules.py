e43e71f75c51fd59619e10022adffcd6976ed109
Abhirami kannan <85607268+Abhiramikannan@users.noreply.github.com>
2025-12-01 11:27:35 +0530

    Update 02_modules.py

--- PYTHON/python_functions/02_modules.py
+++ PYTHON/python_functions/02_modules.py
@@ -1,3 +1,5 @@
+#https://www.youtube.com/watch?v=vPLaFFNGe7A&list=PLsyeobzWxl7omDoEYrrf3oXvXxa6MPgek&index=16
+
 #module in python is like we can use in any programs.. which contains lots of functions.
 #if u want to find square root of a number you can use sqrt() function by importing math module
 #module contains many buildin functions,variables .. that can be used in program any time by importing it.
@@ -14,4 +16,33 @@ print(b)
 
 
 
-https://www.youtube.com/playlist?list=PLsyeobzWxl7omDoEYrrf3oXvXxa6MPgek
+#floor
+  #will give you the lowest value
+
+#ceil
+  #ceil will give you highest value
+
+#pow:
+  #gives power value
+
+#ceil,floor,pow
+from math import sqrt,ceil,pow,floor
+num=25
+n=7.9
+b=sqrt(num)
+c=ceil(n) 
+d=floor(n)
+e=pow(2,3)  #gives 2 power 3
+print(b)  #square root
+print(c) #ceil
+print(d) #floor
+print(e) #power
+
+
+#allias
+import math as m #as m ..so it will be m 
+a=m.sqrt(25)
+print(a
+
+
+      
