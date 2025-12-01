9718ca93bfce8fb9e5630615a2af0b53e42ba777
Abhirami kannan <85607268+Abhiramikannan@users.noreply.github.com>
2025-12-01 11:14:24 +0530

    Create 02_modules.py

---
+++ PYTHON/python_functions/02_modules.py
@@ -0,0 +1,17 @@
+#module in python is like we can use in any programs.. which contains lots of functions.
+#if u want to find square root of a number you can use sqrt() function by importing math module
+#module contains many buildin functions,variables .. that can be used in program any time by importing it.
+import math
+num=25
+b=math.sqrt(num)
+print(b)
+
+#directly can import using below line and dont want to use math.sqrt(num)
+from math import sqrt # here imported so dont want to write math.sqrt(num)
+num=25
+b=sqrt(num) # no need of math here now
+print(b)
+
+
+
+https://www.youtube.com/playlist?list=PLsyeobzWxl7omDoEYrrf3oXvXxa6MPgek
